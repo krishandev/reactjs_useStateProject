5237ef097e8333fe1053fa6ff1c57eea1defdb37
Krishan <krishandev.0102@gmail.com>
2023-03-13 06:22:20 +0530

    first Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,72 @@
-import logo from './logo.svg';
 import './App.css';
+import { Header } from './Components/Header';
+import TextField from '@mui/material/TextField';
+import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
+import AddIcon from '@mui/icons-material/Add';
+import React, { useState } from 'react';
+import DeleteIcon from '@mui/icons-material/Delete';
+
+
 
 function App() {
+
+  const [form, setForm]=useState({});
+  const [data, setData]=useState([]);
+
+const addData=()=>{
+  setData([...data, form]);
+  setForm({form});
+}
+
+const removeItem=(index)=>{
+let arr=data;
+arr.splice(index, 1);
+setData([...arr]);
+}
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+<Header/>
+
+{/* Form Div Start */}
+
+<div className='form'>
+<Stack spacing={2} direction="row">
+<TextField id="outlined-basic" value={form.name} label="Name" onChange={(event)=>setForm({...form, name: event.target.value})
+} variant="outlined" />
+<TextField id="outlined-basic" value={form.email} onChange={(event)=>setForm({...form, email:event.target.value})} label="Email" variant="outlined" />
+<Button variant="contained" onClick={addData}><AddIcon/></Button>
+
+</Stack>
+</div>
+
+{/* Data Show Div   */}
+
+<div className='data'>
+  <div className='data_val'>
+  <h4>Name</h4>
+  <h4>Email</h4>
+  <h4>Remove</h4>
+  </div>
+  {
+    data.map((element, index)=>{
+     return(
+      <div key={index} className='data_val'>
+  <h4>{element.name}</h4>
+  <h4>{element.email}</h4>
+<Stack>
+  <Button onClick={()=>removeItem(index)} variant="outlined" color="error" className='btn_remove'>
+  <DeleteIcon/>
+</Button>
+</Stack>
+  </div>
+
+     )
+    })
+  }
+</div>
+
+
     </div>
   );
 }
